d554036507f44afae941bf1a5235e37a6ad29d47
Ellenszenves <fallouterdo@gmail.com>
2022-08-25 14:09:00 +0200

    .

--- prometheus-scripts/server-install.sh
+++ prometheus-scripts/server-install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo cp prometheus.service /etc/systemd/system
 sudo cd /opt
-sudo mkdir prometheus
-sudo wget https://github.com/prometheus/prometheus/releases/download/v2.37.0/prometheus-2.37.0.linux-amd64.tar.gz
-sudo tar -xvf prometheus-2.37.0.linux-amd64.tar.gz -C /opt/prometheus
+sudo mkdir /opt/prometheus
+sudo wget -P /opt https://github.com/prometheus/prometheus/releases/download/v2.37.0/prometheus-2.37.0.linux-amd64.tar.gz
+sudo tar -xvf /opt/prometheus-2.37.0.linux-amd64.tar.gz -C /opt/prometheus
 
 sudo useradd prometheus
 sudo chown -R prometheus:prometheus prometheus

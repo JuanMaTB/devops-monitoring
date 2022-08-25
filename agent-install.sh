920f83312dd6147e35d8a283b56b055398805d3a
Ellenszenves <fallouterdo@gmail.com>
2022-08-25 13:19:36 +0200

    agent script

--- agent-install.sh
+++ agent-install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 sudo apt update
 sudo apt-get install zabbix-agent
+
+read -p "Server IP:" server_ip
+sudo sed -i "s/Server=127.0.0.1/Server=$server_ip/" /etc/zabbix/zabbix_agentd.conf
+
 sudo systemctl start zabbix-agent
 sudo systemctl enable zabbix-agent
